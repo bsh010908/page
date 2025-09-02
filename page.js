c9745e05d8bc90a0679cf1d3b99fa54d6f099f36
bsh010908 <bsh010908@gmail.com>
2025-09-02 17:27:44 +0900

    asdsad

--- page.js
+++
@@ -1,3 +0,0 @@
-<a href="https://www.templatemonster.com/website-templates/hotelo-hotel-resort-motel-html-and-bootstrap-responsive-website-template-450617.html" class="back-to-product">
-    <span class="back-to-product-icon"></span>
-      Hotelo - Hotel, Resort, Motel HTML and Bootstrap Responsive Website Template  </a>
