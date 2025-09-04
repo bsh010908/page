bc615a6e40381554cdf5470d224cd5e680079b23
bsh010908 <bsh019098@gmail.com>
2025-09-05 08:02:31 +0900

    jhgjh

--- js/toggle.js
+++ js/toggle.js
@@ -1,15 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const moreText = document.getElementById("moreText");
-  const toggleBtn = document.getElementById("toggleBtn");
+const toggleButtons = document.querySelectorAll(".toggleBtn");
 
-  toggleBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    if (moreText.style.display === "none") {
-      moreText.style.display = "inline";
-      toggleBtn.textContent = "read less";
-    } else {
-      moreText.style.display = "none";
-      toggleBtn.textContent = "read more";
-    }
+  toggleButtons.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+      const post = btn.closest(".post");
+      const moreText = post.querySelector(".moreText");
+
+      if (moreText.style.display === "none" || moreText.style.display === "") {
+        moreText.style.display = "inline";
+        btn.textContent = "read less";
+      } else {
+        moreText.style.display = "none";
+        btn.textContent = "read more";
+      }
+    });
   });
-});
